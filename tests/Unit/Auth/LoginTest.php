c8b12e7f83faf1abd862a3a9d23aba8928fa9860
saad-alabyad78 <saadalabyad78@gmail.com>
2024-04-17 14:58:45 +0300

    test auth folder

---
+++ tests/Unit/Auth/LoginTest.php
@@ -0,0 +1,64 @@
+<?php
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_login_with_correct_credentials()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['access_token']);
+    }
+
+    public function test_login_with_incorrect_credentials()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'wrong_password',
+        ]);
+
+        $response->assertStatus(422)
+        ->assertJson([
+        'message' => 'The selected email is invalid.',
+        'errors' => [
+            'email' => [
+                'The selected email is invalid.'
+            ]
+        ]
+    ]);
+    }
+
+    public function test_login_with_unverified_email()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            // لا تحديد مفتاح محدد هنا
+        ]);
+    }
+}
