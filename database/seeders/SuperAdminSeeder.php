52e99a8fc85f17f691183ba678f79ae77d1fc8d0
saad-alabyad78 <saadalabyad78@gmail.com>
2024-04-21 20:45:49 +0300

    yaser super admin

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -3,10 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\SuperAdmin;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -15,18 +15,30 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
+        $users = 
+        [
+            User::create([
             'first_name' => 'saad' , 
             'last_name' => 'alabyad' ,
             'email' => 'saadalabyad78@gmail.com' ,
             'email_verified_at' => '2000-11-11' ,
             'password' => Hash::make('12345678') , 
-        ]);
+        ]) ,
+        
+            User::create([
+            'first_name' => 'ياسر' , 
+            'last_name' => 'جمال الدين' ,
+            'email' => 'yasserjamalaldeen@gmail.com' ,
+            'email_verified_at' => '2000-11-11' ,
+            'password' => Hash::make('12345678') , 
+        ]) 
+        ] ;
         
 
-        $super = SuperAdmin::create() ;
-
-        $super->user()->save($user) ;
-        
+        foreach($users as $user)
+        {
+            $super = SuperAdmin::create()  ;
+            $super->user()->save($user) ;
+        }
     }
 }
