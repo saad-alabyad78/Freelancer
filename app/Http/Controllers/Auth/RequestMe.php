8b643e3db12599574d8a2f2f941a39ceaee388bc
saad-alabyad78 <saadalabyad78@gmail.com>
2024-05-19 21:00:16 +0300

    cache.request.me

--- app/Http/Controllers/Auth/RequestMe.php
+++ app/Http/Controllers/Auth/RequestMe.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Resources\Auth\UserResource;
 /**
  * @group Auth Managment
@@ -22,7 +23,18 @@ class RequestMe extends Controller
      **/
     public function __invoke()
     {
-        return UserResource::make(auth()->user())
-            ->response()->withHeaders(['Accept' => 'application/json']);
+        $userId = auth()->id();
+        $userRole = auth()->user()->role; 
+
+        // Generate a unique cache key based on the user's ID
+        $cacheKey =  $userId . '_' . $userRole;
+
+        // Remember the response in the cache for a given time, e.g., 60 seconds
+        $response = Cache::remember($cacheKey, 60*60*24*7, function () {
+            return UserResource::make(auth()->user());
+        });
+
+        // Return the response with the necessary headers
+        return $response->response()->withHeaders(['Accept' => 'application/json']);
     }
 }
