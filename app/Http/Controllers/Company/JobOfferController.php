4c7849f309bbc85e9756f32e10542e848d27934c
sham97-03 <shamjamous7@example.com>
2024-06-09 15:06:34 +0300

    no changes

--- app/Http/Controllers/Company/JobOfferController.php
+++ app/Http/Controllers/Company/JobOfferController.php
@@ -15,28 +15,33 @@ use App\Http\Requests\Company\DeleteJobOfferRequest;
 use App\Http\Requests\Company\UpdateJobOfferRequest;
 /**
  * @group Company Managment
- * 
+ *
  **/
 class JobOfferController extends Controller
 {
     /**
      * create job offer.
-     * 
+     *
      * @authenticated
-     * 
+     *
      * @apiResource App\Http\Resources\Company\JobOfferResource
      * @apiResourceModel App\Models\JobOffer with=App\Models\Company,App\Models\Skill,App\Models\JobRole
      *
      * @return \App\Http\Resources\Company\JobOfferResource
-     * 
+     *
      */
     public function store(CreateJobOfferRequest $request)
     {
+<<<<<<< Updated upstream
         return $data = $request->validated() ;
             
+=======
+        $data = $request->validated() ;
+
+>>>>>>> Stashed changes
         $data['status'] = JobOfferStatus::PENDING  ;
         $data['company_id'] = auth()->user()->role['id'] ;
-        
+
         $job_offer = JobOffer::Create($data);
 
         $skillables = array_map(function($item) use ($job_offer){
@@ -47,33 +52,33 @@ class JobOfferController extends Controller
                 'required' => $item['required'] ,
             ] ;
         } , $data['skill_ids']);
-        
+
         Skillable::insert($skillables) ;
-        
+
         return JobOfferResource::make($job_offer->load(['company' , 'skills' , 'job_role']));
     }
      /**
      * Update Job Offer .
-     * 
+     *
      * @authenticated
-     * 
+     *
      * @apiResource App\Http\Resources\Company\JobOfferResource
      * @apiResourceModel App\Models\JobOffer with=App\Models\Company,App\Models\Skill,App\Models\JobRole
      *
      * @return \App\Http\Resources\Company\JobOfferResource
-     * 
+     *
      */
     public function update(UpdateJobOfferRequest $request)
     {
         $data = $request->validated() ;
 
-        //chick wither this is his job offer or not 
+        //chick wither this is his job offer or not
 
         $job_offer = JobOffer::findOrFail($data['job_offer_id']);
 
         if($job_offer->company_id != auth()->user()->role['id']){
             return response()->json([
-                'this is not your job offer !' , 
+                'this is not your job offer !' ,
                 422
             ]);
         }
@@ -94,9 +99,9 @@ class JobOfferController extends Controller
 
                 $job_offer->skills()->saveMany($skills) ;
             }
-            
+
             DB::commit();
-            
+
             return JobOfferResource::make($job_offer->load(['company' , 'skills' , 'job_role']));
 
         } catch (\Throwable $th) {
@@ -109,33 +114,37 @@ class JobOfferController extends Controller
     }
     /**
      * Delete Job Offer .
+<<<<<<< Updated upstream
      * 
+=======
+     *
+>>>>>>> Stashed changes
      * @authenticated
      *
      * @return \Illuminate\Http\Response
-     * 
+     *
      */
     public function delete(DeleteJobOfferRequest $request)
     {
         $data = $request->validated() ;
 
-        //chick wither this is his job offer or not 
+        //chick wither this is his job offer or not
 
         $job_offer = JobOffer::findOrFail($data['job_offer_id']);
 
         if($job_offer->company_id != auth()->user()->role['id']){
             return response()->json([
-                'this is not your job offer !' , 
+                'this is not your job offer !' ,
                 422
             ]);
         }
         if($job_offer->status == JobOfferStatus::AVTIVE){
             return response()->json([
-                'you can\' delete active job offer' , 
+                'you can\' delete active job offer' ,
                 422
             ]);
         }
-        
+
         $job_offer->delete() ;
 
         return response()->noContent();
