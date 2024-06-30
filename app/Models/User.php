33194af1ae99356524eefaec94796c50798ca730
saad-alabyad78 <saadalabyad78@gmail.com>
2024-06-30 12:17:33 +0300

    C

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $fillable = [
+        'fcm_token'  ,
         'first_name',
         'email',
         'password',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
 
+            $table->string('fcm_token')->nullable();
+            
             $table->unsignedBigInteger('role_id')->nullable();
             $table->string('role_type')->nullable();
 
