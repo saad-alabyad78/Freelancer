97ede8a941d4d20b03595a6340bfc7adb2db0123
sham97-03 <shamjamous7@example.com>
2024-07-26 19:17:47 +0300

    chat is completed

--- routes/Api/invitation.php
+++ routes/Api/invitation.php
@@ -4,18 +4,43 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvitationController;
 
 // company
-Route::middleware('auth:sanctum', 'verify_email', 'role:company')->group(function () {
-    Route::post('/invitations', [InvitationController::class, 'sendInvitation']);
-    Route::delete('/invitations/{id}', [InvitationController::class, 'deleteInvitation']);
-});
+Route::group(
+    [
+        'middleware' => [
+            'auth:sanctum' ,
+            'verify_email' ,
+            'role:company' ,
+        ],
+    ],
+    function(){
+        Route::post('/invitations', [InvitationController::class, 'sendInvitation']);
+        Route::delete('/invitations/{id}', [InvitationController::class, 'deleteInvitation']);
+    });
 
 // freelancer
-Route::middleware('auth:sanctum', 'verify_email', 'role:freelancer')->group(function () {
-    Route::post('/invitations/{id}/accept', [InvitationController::class, 'acceptInvitation']);
-    Route::post('/invitations/{id}/reject', [InvitationController::class, 'rejectInvitation']);
-});
+Route::group(
+    [
+        'middleware' => [
+            'auth:sanctum' ,
+            'verify_email' ,
+            'role:freelancer' ,
+        ],
+    ],
+    function(){
+        Route::post('/invitations/{id}/accept', [InvitationController::class, 'acceptInvitation']);
+        Route::post('/invitations/{id}/reject', [InvitationController::class, 'rejectInvitation']);
+    });
 
 // both
-Route::middleware('auth:sanctum', 'verify_email', 'role:company,freelancer')->group(function () {
-    Route::get('/invitations', [InvitationController::class, 'getInvitations']);
-});
+Route::group(
+    [
+        'middleware' => [
+            'auth:sanctum' ,
+            'verify_email' ,
+            'role:company,freelancer' ,
+        ],
+    ],
+    function(){
+        Route::get('/invitations', [InvitationController::class, 'getInvitations']);
+
+    });
