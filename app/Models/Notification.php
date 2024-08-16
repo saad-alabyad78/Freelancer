d58f76def348a94f13e712fc5513a6c8c08c837d
saad-alabyad78 <saadalabyad78@gmail.com>
2024-08-16 15:47:21 +0300

    m

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Spatie\Translatable\HasTranslations;
 
 class Notification extends Model
@@ -41,5 +42,10 @@ class Notification extends Model
     public function getAdditionalDataAttribute()
     {
         return isset($this->attributes['additional_data']) ? json_decode($this->attributes['additional_data'], true) : null;
+    }
+
+    public function model():MorphTo
+    {
+        return $this->morphTo() ;
     }
 }
