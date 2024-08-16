27e573bc517c73983a6890c91fb040966e870505
saad-alabyad78 <saadalabyad78@gmail.com>
2024-08-16 16:11:50 +0300

    m

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -16,5 +16,5 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:fresh --force  --seed
+#php artisan migrate:fresh --force  --seed
 #php artisan migrate
