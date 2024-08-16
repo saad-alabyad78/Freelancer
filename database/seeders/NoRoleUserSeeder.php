7667d9bbe06e5de6bb10f349a03571df8a52cdbb
sham97-03 <shamjamous7@example.com>
2024-08-16 17:24:35 +0300

    newww3

--- database/seeders/NoRoleUserSeeder.php
+++ database/seeders/NoRoleUserSeeder.php
@@ -19,7 +19,8 @@ class NoRoleUserSeeder extends Seeder
         for ($i = 0; $i < 50; $i++) {
             $firstName = $faker->firstName;
             $lastName = $faker->lastName;
-            $email = strtolower($firstName . '.' . $lastName . $i . '@example.com');
+            // استخدام الأسماء العربية لإنشاء البريد الإلكتروني باللغة الإنجليزية
+            $email = strtolower($this->transliterate($firstName) . $this->transliterate($lastName) . $i . '@example.com');
             $verifiedAt = $faker->dateTimeBetween('-10 years', 'now')->format('Y-m-d H:i:s');
 
             User::updateOrCreate([
@@ -31,5 +32,22 @@ class NoRoleUserSeeder extends Seeder
                 'password' => Hash::make('12345678'),
             ]);
         }
+    }
+
+    /**
+     * تحويل النص العربي إلى نص لاتيني
+     */
+    private function transliterate($text)
+    {
+        $transliterationTable = [
+            'أ' => 'a', 'ا' => 'a', 'ب' => 'b', 'ت' => 't', 'ث' => 'th', 'ج' => 'j', 'ح' => 'h', 'خ' => 'kh',
+            'د' => 'd', 'ذ' => 'dh', 'ر' => 'r', 'ز' => 'z', 'س' => 's', 'ش' => 'sh', 'ص' => 's',
+            'ض' => 'd', 'ط' => 't', 'ظ' => 'z', 'ع' => 'a', 'غ' => 'gh', 'ف' => 'f', 'ق' => 'q',
+            'ك' => 'k', 'ل' => 'l', 'م' => 'm', 'ن' => 'n', 'ه' => 'h', 'و' => 'w', 'ي' => 'y',
+            'ء' => 'a', 'ى' => 'a', 'ة' => 'h', 'ئ' => 'a', 'ؤ' => 'a', 'إ' => 'a', 'آ' => 'a',
+            ' ' => '_'
+        ];
+
+        return strtr($text, $transliterationTable);
     }
 }
